6b6034de42610c315816511fe82342864b07db06
PradeepKumar1994 <43254499+PradeepKumar1994@users.noreply.github.com>
2020-07-17 01:47:41 -0400

    Requests to Webhdfs

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ Created on Mon May 25 23:30:40 2020
 """
 
 import flask
-from flask import Flask, request, Response
-from database.db import initialize_database
-from database.models import Data_create
-
+from flask import Flask, request, Response, redirect
+#from database.db import initialize_database
+#from database.models import Data_create
+#import pymongo
 from werkzeug.utils import secure_filename
-
-
+import datetime
+import requests
 application = flask.Flask(__name__)
 
 
@@ -28,10 +28,10 @@ application = flask.Flask(__name__)
 #    }
 #]
 
-
-application.config['MONGO_SETTINGS'] = {'host': 'mongodb://localhost/flask-movie-bag'}
-initialize_database(application)
-
+#
+#application.config['MONGO_SETTINGS'] = {'host': 'mongodb://localhost/flask-movie-bag'}
+#initialize_database(application)
+#client = pymongo.MongoClient("mongodb://admin:admin@mongodb-atlas-shard-00-00-afdbx.mongodb.net:27017,mongodb-atlas-shard-00-01-afdbx.mongodb.net:27017,mongodb-atlas-shard-00-02-afdbx.mongodb.net:27017/test?ssl=true&replicaSet=MongoDB-Atlas-shard-0&authSource=admin&retryWrites=true&w=majority")
 
 @application.route('/')
 def hello():
@@ -65,20 +65,103 @@ def update_movie(index):
 
 @application.route('/data/<int:index>', methods=['DELETE'])
 def delete_movie(index):
-    Data.objects.get(id=id).delete()
+    Data_create.objects.get(id=id).delete()
     return '', 200
 
 
 @application.route('/upload')
 def upload_file():
    return flask.render_template('upload.html')
-	
+
+def rename_file(filename):
+    
+    temp = filename.rsplit('.', 1)
+        
+    filename_ = str(datetime.datetime.now())[:-7] + "." + temp[1]
+    
+    return filename_
+         
+        	          
+
 @application.route('/uploader', methods = ['GET', 'POST'])
 def upload_file_():
-   if flask.request.method == 'POST':
-      f = flask.request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+        
+
+    if flask.request.method == 'POST':
+        
+        f = flask.request.files['file']
+      
+        filename_ = rename_file(f.filename)
+        
+        """  hdfs_directory_folder_check - contains the date in yyyy-mm-dd """
+        hdfs_directory_folder_check, hdfs_filename = (filename_).split(' ')[0], (filename_).split(' ')[1]
+        
+        print("hdfs_directory_folder_check", hdfs_directory_folder_check)
+      
+      
+        """  hdfs_directory_api_check - contains the api to 
+        directory(hdfs_directory_folder_check) name """
+        hdfs_directory_api_check = "http://skywalker-G7-7588:50070/webhdfs/v1/test/"+hdfs_directory_folder_check+"/?op=GETFILESTATUS"
+      
+        hdfs_directory_status = str(requests.get(hdfs_directory_api_check)).split(' ')[1][1:4]
+
+        print("Status is : {}".format(hdfs_directory_status))
+        
+        if(hdfs_directory_status == "200"):
+          
+            print("--- Directory exists. Sending the file ---")
+            
+            headers = {'Content-type': 'application/octet-stream'}
+            
+            endpoint_create_file = "http://skywalker-G7-7588:50070/webhdfs/v1/test/"+hdfs_directory_folder_check+"/"+hdfs_filename+"?op=CREATE&overwrite=true&permission=OCTAL" 
+                                                                                                                                        
+            
+#            hdfs_saved_file = requests.put(endpoint_open_file, data = {"uploaded_file": f}) #, headers = headers
+            
+            #hdfs_saved_file = requests.Request('PUT', endpoint_create_file, files=f).prepare().body.decode('ascii')
+            
+            hdfs_saved_file = requests.Request('PUT', endpoint_create_file, files=f)
+            
+            print(hdfs_saved_file.url)
+            
+            #print(hdfs_saved_file.status_code)
+            
+            #print(requests.get(endpoint_open_file))
+          
+        elif(hdfs_directory_status == "404"):
+            
+            print("Directory doesn't exists. Creating the directory under: ", hdfs_directory_folder_check)
+            
+            endpoint_open_dir = "http://skywalker-G7-7588:50070/webhdfs/v1/test/"+hdfs_directory_folder_check+"?op=MKDIRS"
+            
+            #files = {'upload_file': (hdfs_directory_folder_check, f)}
+            
+            hdfs_directory = requests.put(endpoint_open_dir)
+            
+            print("URL from hdfs", hdfs_directory.url)
+            print("hdfs_directory_folder_check:  ", hdfs_directory_folder_check)
+            print("hdfs_filename:  ",hdfs_filename)
+            endpoint_open_file = "http://skywalker-G7-7588:50070/webhdfs/v1/test/"+hdfs_directory_folder_check+"/"+hdfs_filename+"?op=CREATE"
+            
+#            hdfs_saved_file = requests.put(endpoint_open_file, data = f)
+            
+            print(hdfs_saved_file.url)
+            
+            
+      
+        #print("File name: ", hdfs_directory_folder_check)
+      
+        
+        
+        
+      
+        #a = requests.post(endpoint_open)
+        
+        #print(a.url)
+        
+        #f.save(secure_filename(filename_))
+        
+        return 'file uploaded successfully'
 
 
 @application.route('/movies/<id>')
@@ -86,6 +169,12 @@ def get_movie(id):
     movies = Data_create.objects.get(id=id).to_json()
     return Response(movies, mimetype="application/json", status=200)
 
+
+#
+#@application.route('/videofile')
+#def view_file():
+#   return redirect('hdfs://localhost:9000/user/hduser/pradeep.txt')
+#	
 
 application.run()
 
